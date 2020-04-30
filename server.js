93c6ad298d214340bf0d2910716b7af6acc6f023
Siphamandla <mr.smehlomakulu@gmail.com>
2020-04-30 23:39:08 +0200

    add express-validator to user route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init middleware
+// There's no need to install body parser now
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
    res.json({ msg: 'Welcome to the contact keeper API...' })
 );
